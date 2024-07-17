14e2fe45ede100c45b4efae41223b4870751a624
Kirk Brauer <kirkebrauer@gmail.com>
2024-07-17 13:02:20 -0400

    Add Click CLI

---
+++ jumpstarter/cli/main.py
@@ -0,0 +1,23 @@
+"""Main Jumpstarter CLI"""
+import click
+import os
+import sys
+
+from jumpstarter import __version__
+
+
+def version_msg():
+    """Return the Jumpstarter version."""
+    python_version = sys.version
+    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return f"Jumpstarter {__version__} from {location} (Python {python_version})"
+
+
+@click.command(context_settings={"help_option_names": ['-h', '--help']}, no_args_is_help=True)
+@click.version_option(__version__, '-V', '--version', message=version_msg())
+def main():
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
