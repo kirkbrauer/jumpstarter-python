dd039a2f6e54a7774b6d97c442d7da473418aa5c
Kirk Brauer <kirkebrauer@gmail.com>
2024-07-17 21:53:10 +0000

    Move version to subcommand

--- jumpstarter/cli/main.py
+++ jumpstarter/cli/main.py
@@ -1,24 +1,17 @@
 """Main Jumpstarter CLI"""
 import click
-import os
 import sys
-import importlib.metadata
 
 
-# Get the version from pyproject.toml
-__version__ = importlib.metadata.version('jumpstarter')
-
-def version_msg():
-    """Return the Jumpstarter version."""
-    python_version = sys.version
-    location = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return f"Jumpstarter {__version__} from {location} (Python {python_version})"
+from .version import version
 
 
-@click.command(context_settings={"help_option_names": ['-h', '--help']}, no_args_is_help=True)
-@click.version_option(__version__, '-V', '--version', message=version_msg())
+@click.group(context_settings={"help_option_names": ['-h', '--help']}, no_args_is_help=True)
 def main():
-    sys.exit(1)
+    pass
+
+
+main.add_command(version)
 
 
 if __name__ == "__main__":
